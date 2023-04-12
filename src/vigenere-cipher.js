c65b232446cd8d84c33be615e56b71552f88d740
TatarchukVladimir <tatarchuk.vladimir16@gmail.com>
2023-04-12 12:02:02 +0300

    feat: feat solution vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification = true) {
+    this.modification = modification;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // reove line with error and write your code here
+
+  encrypt(str, key) {
+    if (!str || !key) throw new Error('Incorrect arguments!');
+    let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let res = '';
+    let newKey = '';
+    let index = 0;
+
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < str.length; i++) {
+        
+        if (this.alphabet.indexOf(str[i]) == -1) {
+            res += str[i];
+            newKey += ' ';
+            continue
+          }
+        newKey += key[index % key.length]
+        index++;
+        res += this.alphabet[(this.alphabet.length + this.alphabet.indexOf(str[i]) + this.alphabet.indexOf(newKey[i])) % this.alphabet.length];
+    }
+    return (this.modification) ? res : res.split('').reverse().join('')
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error('Incorrect arguments!');
+    let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let res = '';
+    let newKey = '';
+    let index = 0;
+
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < str.length; i++) {
+        
+        if (this.alphabet.indexOf(str[i]) == -1) {
+            res += str[i];
+            newKey += ' ';
+            continue
+          }
+        newKey += key[index % key.length]
+        index++;
+        res += this.alphabet[(this.alphabet.length + this.alphabet.indexOf(str[i]) - this.alphabet.indexOf(newKey[i])) % this.alphabet.length];
+    }
+    return (this.modification) ? res : res.split('').reverse().join('');
   }
 }
 
